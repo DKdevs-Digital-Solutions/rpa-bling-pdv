dacb867e6fafd2c41db1e24aa338f7c28721b07c
Dronasci <122463183+intalkconnect@users.noreply.github.com>
2026-02-09 14:42:16 -0300

    Delete routes directory

--- routes/auth.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { start, callback } = require("../controllers/auth.controller");
-
-router.get("/start", start);
-router.get("/callback", callback);
-
-module.exports = router;
--- routes/sync.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { sync, status } = require("../controllers/sync.controller");
-
-router.post("/", sync);
-router.get("/status", status);
-
-module.exports = router;
